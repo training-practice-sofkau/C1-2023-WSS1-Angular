1dcb15242e67f7888807dae9fa0d28c948defcc3
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-02-07 18:08:58 -0500

    Minimal configuration to work

--- src/app/components/album-list/album-list.component.ts
+++ src/app/components/album-list/album-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ALBUMS } from '../../mocks/albums.mock';
+import { IAlbum } from '../../models/album.interface';
 
 @Component({
   selector: 'app-album-list',
   templateUrl: './album-list.component.html',
   styleUrls: ['./album-list.component.scss']
 })
-export class AlbumListComponent {
+export class AlbumListComponent implements OnInit{
+  @Input() param: string = "";
+  albums: IAlbum[] = ALBUMS;
+  l_albums: IAlbum[] = [];
 
+  results: number = 0;
+  filter: string = "title";
+  p:number = 0;
+
+  ngOnInit(): void {
+    this.l_albums = this.albums;
+    this.results = this.l_albums.length;
+  }
+
+  ngOnSearch(){
+    
+  }
 }
