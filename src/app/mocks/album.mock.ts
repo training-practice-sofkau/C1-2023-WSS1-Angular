6cc2eb7e356b78bb815a320d6c3942f727885365
isan-san <isan.9.9f@gmail.com>
2023-02-07 15:42:23 -0500

    Finished filling Albumn and Artist mocks

---
+++ src/app/mocks/album.mock.ts
@@ -0,0 +1,74 @@
+import { IAlbum } from '../models/album.interface';
+
+export const ALBUM: IAlbum[] = [
+  {
+    title: 'TAMARA',
+    release_date: '2022',
+    artist: 'El Mañas',
+    genre: 'RAP',
+    number_of_songs: 6,
+  },
+  {
+    title: 'Círculo Vicioso',
+    release_date: '2017',
+    artist: 'Granuja',
+    genre: 'RAP',
+    number_of_songs: 20,
+  },
+  {
+    title: 'Un Minuto',
+    release_date: '2020',
+    artist: 'Gambeta',
+    genre: 'RAP',
+    number_of_songs: 1,
+  },
+  {
+    title: 'The Loock Of Love',
+    release_date: '2022',
+    artist: 'Sr Pablo',
+    genre: 'RAP',
+    number_of_songs: 0,
+  },
+  {
+    title: 'The Car',
+    release_date: '2022',
+    artist: 'Arctic Monkeys',
+    genre: 'Rock',
+    number_of_songs: 10,
+  },
+  {
+    title: 'Lo Esencial',
+    release_date: '2008',
+    artist: 'Soda Stereo',
+    genre: 'Rock',
+    number_of_songs: 15,
+  },
+  {
+    title: 'Soda Stero',
+    release_date: '1984',
+    artist: 'Soda Stereo',
+    genre: 'Rock',
+    number_of_songs: 11,
+  },
+  {
+    title: 'Aranjuez',
+    release_date: '2022',
+    artist: 'Alcolirikoz',
+    genre: 'RAP',
+    number_of_songs: 10,
+  },
+  {
+    title: 'Vida',
+    release_date: '2011',
+    artist: 'Canserbero',
+    genre: 'RAP',
+    number_of_songs: 10,
+  },
+  {
+    title: 'Muerte',
+    release_date: '2009',
+    artist: 'Canserbero',
+    genre: 'RAP',
+    number_of_songs: 10,
+  },
+];
