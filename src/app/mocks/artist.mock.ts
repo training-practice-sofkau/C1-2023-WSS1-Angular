816754994e891623c7fca75dd2333e5ff2022603
Julian Rojas <julianrojasgeo@gmail.com>
2023-02-07 13:39:21 -0500

    Artist mock complete

--- src/app/mocks/artist.mock.ts
+++ src/app/mocks/artist.mock.ts
@@ -1,20 +1,84 @@
 import { IArtist } from "../models/artist.interface";
 
 export const ARTISTS: IArtist[] = [
-    {
-        id: 1,
-        name: 'Bruno Mars',
-        country: 'USA',
-        age: 30,
-        year_debut: 2004,
-        total_albums: 12
-    },
-    {
-        id: 2,
-        name: 'Jessie Ware',
-        country: 'UK',
-        age: 38,
-        year_debut: 2017,
-        total_albums: 4
-    }
+  {
+    id: 1,
+    name: 'Bruno Mars',
+    country: 'USA',
+    age: 30,
+    year_debut: 2004,
+    total_albums: 12
+  },
+  {
+    id: 2,
+    name: 'Jessie Ware',
+    country: 'UK',
+    age: 38,
+    year_debut: 2017,
+    total_albums: 4
+  },
+  {
+    id: 3,
+    name: 'Paul McCartney',
+    country: 'UK',
+    age: 80,
+    year_debut: 1956,
+    total_albums: 15
+  },
+  {
+    id: 4,
+    name: 'Marc Anthony',
+    country: 'USA',
+    age: 54,
+    year_debut: 1995,
+    total_albums: 8
+  },
+  {
+    id: 5,
+    name: 'Natalia Lafourcade',
+    country: 'MX',
+    age: 38,
+    year_debut: 2000,
+    total_albums: 5
+  },
+  {
+    id: 6,
+    name: 'Sting',
+    country: 'UK',
+    age: 71,
+    year_debut: 1970,
+    total_albums:12
+  },
+  {
+    id: 7,
+    name: 'Bon Jovi',
+    country: 'USA',
+    age: 60,
+    year_debut: 1978,
+    total_albums: 12
+  },
+  {
+    id: 8,
+    name: 'Robbie Williams',
+    country: 'UK',
+    age: 48,
+    year_debut: 1990,
+    total_albums: 9
+  },
+  {
+    id: 9,
+    name: 'Julieta Venegas',
+    country: 'UK',
+    age: 53,
+    year_debut:1995,
+    total_albums: 7
+  },
+  {
+    id: 10,
+    name: 'Mick Jagger',
+    country: 'UK',
+    age: 79,
+    year_debut: 1980,
+    total_albums: 9
+  }
 ]
