c1a56960772a7d51a95d13718be575ce3e9040d5
Juangmcz <juangui-089@hotmail.com>
2023-02-07 14:27:45 -0500

    Some variable names were changed.

--- src/app/mocks/artist.mock.ts
+++ src/app/mocks/artist.mock.ts
@@ -1,20 +1,20 @@
-import { IArtist } from "../models/artist.interface";
+import { IArtist } from '../models/artist.interface';
 
 export const ARTISTS: IArtist[] = [
-    {
-        id: 1,
-        name: 'Bruno Mars',
-        country: 'USA',
-        age: 30,
-        year_debut: 2004,
-        total_albums: 12
-    },
-    {
-        id: 2,
-        name: 'Jessie Ware',
-        country: 'UK',
-        age: 38,
-        year_debut: 2017,
-        total_albums: 4
-    }
-]
+  {
+    id: 1,
+    name: 'Bruno Mars',
+    country: 'USA',
+    age: 30,
+    debutYear: 2004,
+    totalAlbums: 12,
+  },
+  {
+    id: 2,
+    name: 'Jessie Ware',
+    country: 'UK',
+    age: 38,
+    debutYear: 2017,
+    totalAlbums: 4,
+  },
+];
