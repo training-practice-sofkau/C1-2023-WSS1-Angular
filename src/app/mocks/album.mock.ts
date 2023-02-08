c1cc2d044f0fd0af99971f43060f79b725819d13
Difsan <difsanchezru@gmail.com>
2023-02-08 00:11:18 -0500

    album mock and interface added

---
+++ src/app/mocks/album.mock.ts
@@ -0,0 +1,12 @@
+import { IAlbum } from "../models/album.interface";
+
+export const ALBUM: IAlbum[]=[
+    {
+        id: 1,
+        title: "24K Magic",
+        genre: "Pop",
+        release_date: "11-07-2023",
+        songs: 9,
+        artist: "Bruno Mars"
+    }
+]
