47a6534f7f82d9515570654808869a01e680a740
Santiago Ramirez Cano <santiago.ramirezc26@gmail.com>
2023-02-08 13:35:13 -0500

    Implementation of the album-list component, added img to each item list, responsive design and pagination per list

--- src/app/components/album-list/album-list.component.ts
+++ src/app/components/album-list/album-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ALBUMS } from 'src/app/mocks/album.mock';
+import { IAlbum } from 'src/app/models/album.interface';
 
 @Component({
   selector: 'app-album-list',
@@ -6,5 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./album-list.component.scss']
 })
 export class AlbumListComponent {
+
+  @Input() param: string = "";
+
+  //TO-DO: Define a variable that will store the information
+  l_albums: IAlbum[] = [];
+
+  results: number = 0;
+
+  p: number = 1;
+  count: number = 3;
+
+  ngOnInit(): void {
+    this.l_albums = ALBUMS;
+    this.results = this.l_albums.length;
+  }
+
+  //TO-DO: Create a function that based of param it will show n-results
+  ngOnSearchByTitle(param: string){
+    this.l_albums = this.l_albums.filter((artist) => artist.title.toLowerCase().includes(param.toLowerCase())).sort((a, b) => (a.title > b.title) ? 1 : -1);
+    this.results = this.l_albums.length;
+    this.p = 1;
+  }
+
+  ngOnSearchByGenre(param: string){
+    this.l_albums = this.l_albums.filter((artist) => artist.genre.toLowerCase().includes(param.toLowerCase())).sort((a, b) => (a.genre > b.genre) ? 1 : -1);
+    this.results = this.l_albums.length;
+    this.p = 1;
+  }
+
+  ngOnSearchBySongs(param: string){
+    this.l_albums = this.l_albums.filter((artist) => artist.number_of_songs.toString().includes(param)).sort((a, b) => (a.number_of_songs > b.number_of_songs) ? 1 : -1);
+    this.results = this.l_albums.length;
+    this.p = 1;
+  }
+
+  ngInputReset(){
+    this.l_albums = ALBUMS;
+    this.results = this.l_albums.length;
+  }
 
 }
---
+++ src/app/models/album.interface.ts
@@ -0,0 +1,8 @@
+export interface IAlbum {
+  title: string,
+  genre: string,
+  release_date: string,
+  number_of_songs: number,
+  artist: string,
+  img: string,
+}
