3e8fa9caa266cd71711a8d62b198490afd8e68f7
isan-san <isan.9.9f@gmail.com>
2023-02-08 11:01:38 -0500

    Filter acording to de search param and show results

--- src/app/components/album-list/album-list.component.ts
+++ src/app/components/album-list/album-list.component.ts
@@ -10,16 +10,66 @@ import { IAlbum } from 'src/app/models/album.interface';
 export class AlbumListComponent {
   //TO-DO: Define a variable that will store the information
   albums: IAlbum[] = [];
+  searchResulst: IAlbum[] = [];
   searchParam: string = '';
   results: number = 0;
 
   ngOnInit(): void {
     this.albums = ALBUMS;
+    this.searchResulst = this.albums;
     this.results = this.albums.length;
   }
 
   //TO-DO: Create a function that based of param it will show n-results
   ngOnSearch(param: string, typeSearch: string) {
-    console.log(param + typeSearch);
+    console.log(param + ' ' + typeSearch);
+    switch (typeSearch) {
+      case 'title':
+        this.searchResulst = this.searchByTitle(this.albums, param);
+        break;
+      case 'genre':
+        this.searchResulst = this.searchByGenre(this.albums, param);
+        break;
+      case 'artist':
+        this.searchResulst = this.searchByArtist(this.albums, param);
+        break;
+      default:
+        console.log('Error filtering the list');
+        break;
+    }
+    console.table(this.searchResulst);
+  }
+  searchByTitle(albums: IAlbum[], param: string) {
+    return albums
+      .filter((album) =>
+        album.title.toLowerCase().includes(param.toLowerCase())
+      )
+      .sort((a, b) => {
+        a.title.toLowerCase();
+        b.title.toLowerCase();
+        return a.title.localeCompare(b.title);
+      });
+  }
+  searchByGenre(albums: IAlbum[], param: string) {
+    return albums
+      .filter((album) =>
+        album.genre.toLowerCase().includes(param.toLowerCase())
+      )
+      .sort((a, b) => {
+        a.genre.toLowerCase();
+        b.genre.toLowerCase();
+        return a.genre.localeCompare(b.genre);
+      });
+  }
+  searchByArtist(albums: IAlbum[], param: string) {
+    return albums
+      .filter((album) =>
+        album.artist.toLowerCase().includes(param.toLowerCase())
+      )
+      .sort((a, b) => {
+        a.artist.toLowerCase();
+        b.artist.toLowerCase();
+        return a.artist.localeCompare(b.artist);
+      });
   }
 }
--- src/app/components/artist-list/artist-list.component.ts
+++ src/app/components/artist-list/artist-list.component.ts
@@ -10,16 +10,62 @@ import { IArtist } from '../../models/artist.interface';
 export class ArtistListComponent implements OnInit {
   //TO-DO: Define a variable that will store the information
   artists: IArtist[] = [];
+  searchResulst: IArtist[] = [];
   searchParam: string = '';
   results: number = 0;
 
   ngOnInit(): void {
     this.artists = ARTISTS;
+    this.searchResulst = this.artists;
     this.results = this.artists.length;
   }
 
-  //TO-DO: Create a function that based of param it will show n-results
-  ngOnSearch(param: string, typeSearch: string) {
-    console.log(param);
+  ngOnSearch(param : any, typeSearch: string) {
+    console.log(param + ' ' + typeSearch);
+    switch (typeSearch) {
+      case 'name':
+        this.searchResulst = this.searchByTitle(this.artists, param);
+        break;
+      case 'country':
+        this.searchResulst = this.searchByGenre(this.artists, param);
+        break;
+      case 'age':
+        this.searchResulst = this.searchByArtist(this.artists, param);
+        break;
+      default:
+        console.log('Error filtering the list');
+        break;
+    }
+    console.table(this.searchResulst);
   }
+  searchByTitle(artists: IArtist[], param: string) {
+    return artists
+      .filter((album) =>
+        album.name.toLowerCase().includes(param.toLowerCase())
+      )
+      .sort((a, b) => {
+        a.name.toLowerCase();
+        b.name.toLowerCase();
+        return a.name.localeCompare(b.name);
+      });
+  }
+  searchByGenre(artists: IArtist[], param: string) {
+    return artists
+      .filter((album) =>
+        album.country.toLowerCase().includes(param.toLowerCase())
+      )
+      .sort((a, b) => {
+        a.country.toLowerCase();
+        b.country.toLowerCase();
+        return a.country.localeCompare(b.country);
+      });
+  }
+  searchByArtist(artists: IArtist[], param: number) {
+    return artists
+      .filter((album) =>
+        album.age==param
+      )
+      .sort((a, b) => a.age-b.age);
+  }
+
 }
