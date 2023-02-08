49fbdabfb3a400098a92b460b268a74e8140b381
Diego Becerra <diegofer_1110@hotmail.com>
2023-02-08 12:46:00 -0500

    adding info for artist mock

--- src/app/mocks/artist.mock.ts
+++ src/app/mocks/artist.mock.ts
@@ -42,5 +42,50 @@ export const ARTISTS: IArtist[] = [
         year_debut: 2016,
         total_albums: 2,
         link:'https://www.billboard.com/wp-content/uploads/2022/07/sound-bizarrap-billboard-2022-guido-adler-1548.jpg?w=942&h=623&crop=1'
+    },
+    {
+        id: 6,
+        name: 'Shakira',
+        country: 'Colombia',
+        age: 45,
+        year_debut: 1998,
+        total_albums: 14,
+        link:'https://www.semana.com/resizer/2y7yPoaOM0jaqpNEt9lCkJFDadc=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/6NHH46JDN5GW5NCQGYXTFQJTK4.png'
+    },
+    {
+        id: 7,
+        name: 'Sia',
+        country: 'Australia',
+        age: 47,
+        year_debut: 1990,
+        total_albums: 16,
+        link:'https://www.musicmundial.com/wp-content/uploads/2022/01/Sia-revela-que-fue-a-rehabilitacion-por-tendencias-suicidas-tras-las-criticas-a-su-pelicula-768x445.jpeg'
+    },
+    {
+        id: 8,
+        name: 'Rihana',
+        country: 'Barbados',
+        age: 34,
+        year_debut: 2003,
+        total_albums: 9,
+        link:'https://www.clarin.com/img/2022/09/26/Lgo8GjmWv_1256x620__1.jpg'
+    },
+    {
+        id: 9,
+        name: 'Marshmello',
+        country: 'USA',
+        age: 30,
+        year_debut: 2015,
+        total_albums: 5,
+        link:'https://www.foodandwine.com/thmb/bpX4z2DLpwcKrUDagmmYJuYPKEM=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cooking-with-marshmello-ft-blog0218-e5c9d7246c03469cb70ae163852e1bd6.jpg'
+    },
+    {
+        id: 10,
+        name: 'Melendi',
+        country: 'España',
+        age: 44,
+        year_debut: 2001,
+        total_albums: 11,
+        link:'https://s1.ticketm.net/dam/a/cee/e3696b74-d7d1-45d3-8dab-4c89282cecee_1735131_TABLET_LANDSCAPE_LARGE_16_9.jpg'
     }
 ]
