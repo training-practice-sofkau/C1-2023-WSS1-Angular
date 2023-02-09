73d46271498ce704d9e89c1ba0f1a86eeaaabb3a
Diego Becerra <diegofer_1110@hotmail.com>
2023-02-09 16:50:58 -0500

    setting services for artist

--- src/app/services/artist-services/artist.service.ts
+++ src/app/services/artist-services/artist.service.ts
@@ -1,30 +1,69 @@
 import { Injectable } from '@angular/core';
-import { Observable} from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { ARTISTS } from 'src/app/mocks/artist.mock';
 import { IArtist } from 'src/app/models/artist.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArtistService {
-
-  constructor() { }
+  constructor() {}
 
   //TO-DO: All the functionalities related to artist
   getAll(): Observable<IArtist[]> {
-  
-    let obsArtist: Observable<IArtist[]> = new Observable(observer => {
+    let obsArtist: Observable<IArtist[]> = new Observable((observer) => {
       observer.next(ARTISTS);
       observer.complete();
     });
 
     return obsArtist;
-
   }
 
-  /*getByName(){
+  getByStartWithString(myParam:string,myValue:string): Observable<IArtist[]> {
+    return this.getAll().pipe(
+      map((ar) => ar.filter(ar=>ar[myParam as keyof IArtist]
+        ?.toString()
+        .toLocaleLowerCase()
+        .startsWith(myValue.toLocaleLowerCase())).sort((a, b) => (a[myParam as keyof IArtist]?.valueOf() ||
+      1 )> (b[myParam as keyof IArtist]?.valueOf() || 1)
+        ? 1
+        : -1))
+    );
+  }
 
-  }*/
+  getByNoStartWithString(myParam:string,myValue:string): Observable<IArtist[]> {
+    return this.getAll().pipe(
+      map((ar) => ar.filter(ar=>!ar[myParam as keyof IArtist]
+        ?.toString()
+        .toLocaleLowerCase()
+        .startsWith(myValue.toLocaleLowerCase())).sort((a, b) => (a[myParam as keyof IArtist]?.valueOf() ||
+      1 )> (b[myParam as keyof IArtist]?.valueOf() || 1)
+        ? 1
+        : -1))
+    );
+  }
+
+  getGreaterThan(myParam:string,myValue:string): Observable<IArtist[]>{
+    return this.getAll().pipe(
+      map((ar) => ar.filter(ar=>
+        (ar[myParam as keyof IArtist]
+        ?.valueOf() || 0) > myValue).sort((a, b) => (a[myParam as keyof IArtist]?.valueOf() ||
+      1 )> (b[myParam as keyof IArtist]?.valueOf() || 1)
+        ? -1
+        : 1))
+    );
+  }
+
+  getLessThan(myParam:string,myValue:string): Observable<IArtist[]>{
+    return this.getAll().pipe(
+      map((ar) => ar.filter(ar=>
+        (ar[myParam as keyof IArtist]
+        ?.valueOf() || 0) <= myValue).sort((a, b) => (a[myParam as keyof IArtist]?.valueOf() ||
+      1 )> (b[myParam as keyof IArtist]?.valueOf() || 1)
+        ? -1
+        : 1))
+    );
+  }
 
   /*getByCountry(){
 
