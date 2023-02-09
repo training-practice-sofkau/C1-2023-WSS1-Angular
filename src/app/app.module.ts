385f419f8b53e5a6611724ca871e52575a83caf7
Juangmcz <juangui-089@hotmail.com>
2023-02-09 16:17:53 -0500

    Some imports were made.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ArtistListComponent } from './components/artist-list/artist-list.component';
-import { AlbumListComponent } from './components/album-list/album-list.component';
 import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { AlbumListComponent } from './components/album-list/album-list.component';
+import { ArtistListComponent } from './components/artist-list/artist-list.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, ArtistListComponent, AlbumListComponent],
-  imports: [FormsModule, BrowserModule, NgxPaginationModule],
+  declarations: [
+    AppComponent,
+    ArtistListComponent,
+    AlbumListComponent,
+    HomePageComponent,
+    NotFoundComponent,
+  ],
+  imports: [FormsModule, BrowserModule, NgxPaginationModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
