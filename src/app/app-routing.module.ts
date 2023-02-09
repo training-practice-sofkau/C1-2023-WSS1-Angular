3f607773a50c41771502e0c9162e61c3b79b19b0
Juangmcz <juangui-089@hotmail.com>
2023-02-09 16:04:55 -0500

    The routing-module was created.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AlbumListComponent } from './components/album-list/album-list.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { ArtistListComponent } from './components/artist-list/artist-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomePageComponent,
+    children: [
+      {
+        path: 'artist',
+        component: ArtistListComponent,
+      },
+      {
+        path: 'album',
+        component: AlbumListComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
