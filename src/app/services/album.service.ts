76a583f9e0fba20120476744e8d15a1db793a0ed
isan-san <isan.9.9f@gmail.com>
2023-02-09 21:43:55 -0500

    Develope album service methods

--- src/app/services/album.service.ts
+++ src/app/services/album.service.ts
@@ -16,9 +16,92 @@ export class AlbumService {
       observer.next(ALBUMS);
       observer.complete();
     });
+    return obsArtist;
+  }
+
+  filterByTitle(
+    artists: IAlbum[],
+    operator: number,
+    parameter: string
+  ): Observable<IAlbum[]> {
+    let obsArtist: Observable<IAlbum[]> = new Observable((observer) => {
+      let artistsFiltered: IAlbum[] = artists
+        .filter((artist) =>
+          this.checkStringCoincidence(artist.title, parameter, operator)
+        )
+        .sort((a, b) => {
+          a.title.toLowerCase();
+          b.title.toLowerCase();
+          return a.title.localeCompare(b.title);
+        });
+      observer.next(artistsFiltered);
+      observer.complete();
+    });
 
     return obsArtist;
-
   }
+
+  filterByGenre(
+    artists: IAlbum[],
+    operator: number,
+    parameter: string
+  ): Observable<IAlbum[]> {
+    let obsArtist: Observable<IAlbum[]> = new Observable((observer) => {
+      let artistsFiltered: IAlbum[] = artists
+        .filter((artist) =>
+          this.checkStringCoincidence(artist.genre, parameter, operator)
+        )
+        .sort((a, b) => {
+          a.genre.toLowerCase();
+          b.genre.toLowerCase();
+          return a.genre.localeCompare(b.genre);
+        });
+      observer.next(artistsFiltered);
+      observer.complete();
+    });
+
+    return obsArtist;
+  }
+
+  filterByArtist(
+    artists: IAlbum[],
+    operator: number,
+    parameter: string
+  ): Observable<IAlbum[]> {
+    let obsArtist: Observable<IAlbum[]> = new Observable((observer) => {
+      let artistsFiltered: IAlbum[] = artists
+        .filter((artist) =>
+          this.checkStringCoincidence(artist.artist, parameter, operator)
+        )
+        .sort((a, b) => {
+          a.artist.toLowerCase();
+          b.artist.toLowerCase();
+          return a.artist.localeCompare(b.artist);
+        });
+      observer.next(artistsFiltered);
+      observer.complete();
+    });
+
+    return obsArtist;
+  }
+
+  checkStringCoincidence(
+    word: string,
+    param: string,
+    operator: number
+  ): boolean | undefined {
+    if (operator == 1) {
+      return (
+        word.toLocaleLowerCase().slice(0, param.length) === param.toLowerCase()
+      );
+    } else if (operator == 4) {
+      return (
+        word.toLocaleLowerCase().slice(0, param.length) != param.toLowerCase()
+      );
+    } else {
+      return undefined;
+    }
+  }
+
 
 }
