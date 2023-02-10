d9be4eec413265bf307f1bdaba00cb7e6e129856
isan-san <isan.9.9f@gmail.com>
2023-02-10 12:46:54 -0500

    Create new components for artist and album cards and implement them

--- src/app/components/pure/album-card/album-card/album-card.component.ts
+++ src/app/components/pure/album-card/album-card/album-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IAlbum } from 'src/app/models/album.interface';
 
 @Component({
   selector: 'app-album-card',
   templateUrl: './album-card.component.html',
-  styleUrls: ['./album-card.component.scss']
+  styleUrls: ['./album-card.component.scss'],
 })
 export class AlbumCardComponent {
-
+  @Input() album: IAlbum = {
+    artist: 'n/a',
+    genre: 'n/a',
+    number_of_songs: 0,
+    release_date: 'n/a',
+    title: 'n/a',
+  };
 }
--- src/app/components/pure/artist-card/artist-card/artist-card.component.ts
+++ src/app/components/pure/artist-card/artist-card/artist-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IArtist } from 'src/app/models/artist.interface';
 
 @Component({
   selector: 'app-artist-card',
   templateUrl: './artist-card.component.html',
-  styleUrls: ['./artist-card.component.scss']
+  styleUrls: ['./artist-card.component.scss'],
 })
 export class ArtistCardComponent {
-
+  @Input() artist: IArtist = {
+    age: 0,
+    country: "n/a",
+    id: 0,
+    name: "n/a",
+    total_albums: 0,
+    year_debut: 0,
+  };
 }
