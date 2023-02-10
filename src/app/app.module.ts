025d1d90593f7cb6ce610be98e6778fd73c74c60
William Serna <williamsernac@hotmail.com>
2023-02-09 23:52:59 -0500

    Imported necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ArtistListComponent } from './components/artist-list/artist-list.component';
-import { AlbumListComponent } from './components/album-list/album-list.component';
-import { FormsModule } from '@angular/forms';
+import { AlbumListComponent } from './album-list/album-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from '@angular/material/select';
+import {NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,11 @@ import { HomePageComponent } from './components/home-page/home-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgxPaginationModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
