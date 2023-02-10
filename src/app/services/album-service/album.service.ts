d77be4700a303b2577fcdaea022750431b9198fe
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2023-02-09 19:19:21 -0500

    Defines the functionality on albums service

--- src/app/services/album-service/album.service.ts
+++ src/app/services/album-service/album.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IAlbum } from 'src/app/models/album.interface';
+import { ALBUMS } from '../../mocks/albums.mock';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,73 @@ import { Injectable } from '@angular/core';
 export class AlbumService {
 
   constructor() { }
+
+  getAll(): Observable<IAlbum[]> {
+    return new Observable((observer) => {
+      observer.next(ALBUMS);
+      observer.complete();
+    });
+  }
+
+  filterByTitle(searchingText: string): Observable<IAlbum[]> {
+    return new Observable((observer) => {
+      observer.next(
+        ALBUMS.sort((a, b) => a.title.localeCompare(b.title)).filter((album) =>
+          album.title.toLowerCase().startsWith(searchingText.toLowerCase())
+        )
+      );
+      observer.complete();
+    });
+  }
+
+  filterByArtist(searchingText: string): Observable<IAlbum[]> {
+    return new Observable((observer) => {
+      observer.next(
+        ALBUMS.sort((a, b) => a.artist.localeCompare(b.artist)).filter((album) =>
+          album.artist.toLowerCase().startsWith(searchingText.toLowerCase())
+        )
+      );
+      observer.complete();
+    });
+  }
+
+  filterByYear(searchingText: string): Observable<IAlbum[]> {
+    return new Observable((observer) => {
+      searchingText === ''
+        ? observer.next(ALBUMS.sort((a, b) => b.relase_date - a.relase_date))
+        : observer.next(
+            ALBUMS.sort((a, b) => b.relase_date - a.relase_date).filter(
+              (album) => album.relase_date === parseInt(searchingText)
+            )
+          );
+      observer.complete();
+    });
+  }
+
+  filterMinumumSongs(searchingText: string): Observable<IAlbum[]> {
+    return new Observable((observer) => {
+      searchingText === ''
+        ? observer.next(ALBUMS.sort((a, b) => b.number_of_songs - a.number_of_songs))
+        : observer.next(
+            ALBUMS.sort((a, b) => b.number_of_songs - a.number_of_songs).filter(
+              (album) => album.number_of_songs >= parseInt(searchingText)
+            )
+          );
+      observer.complete();
+    });
+  }
+
+  filterNonSingles(searchingText: string): Observable<IAlbum[]> {
+    return new Observable((observer) => {
+      searchingText === ''
+        ? observer.next(ALBUMS.sort((a, b) => b.number_of_songs - a.number_of_songs))
+        : observer.next(
+            ALBUMS.sort((a, b) => b.number_of_songs - a.number_of_songs).filter(
+              (album) => album.number_of_songs > 1
+            )
+          );
+      observer.complete();
+    });
+  }
+
 }
