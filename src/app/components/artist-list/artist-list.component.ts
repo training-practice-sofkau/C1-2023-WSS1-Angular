2775caa50c559cf4bbde64664c84adb0058cc9d2
William Serna <williamsernac@hotmail.com>
2023-02-09 23:56:06 -0500

    Artist components implemented with services

--- src/app/components/artist-list/artist-list.component.ts
+++ src/app/components/artist-list/artist-list.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ARTISTS } from 'src/app/mocks/artist.mock';
 import { IArtist } from 'src/app/models/artist.interface';
 import { ArtistService } from 'src/app/services/artist-services/artist.service';
+
+
+
 
 @Component({
   selector: 'app-artist-list',
@@ -13,21 +17,37 @@ export class ArtistListComponent implements OnInit{
 
   constructor(private service: ArtistService) {}
 
-  @Input() param: string = "";
+  param: string = "";
+  choice: string = "";
+  page: number = 0;
+
+
+
 
   //TO-DO: Define a variable that will store the information
   l_artists: IArtist[] = [];
-
   results: number = 0;
-  
+
   ngOnInit(): void {
     this.service.getAll().subscribe((artist) => this.l_artists = artist);
     this.results = this.l_artists.length;
   }
 
   //TO-DO: Create a function that based of param it will show n-results
-  ngOnSearch(param: string, typeSearch: string){
-    console.log(param)
+  ngOnSearch(choice : string){
+    console.log(this.param)
+    console.log(this.choice)
+    if (choice == 'nameInclude')this.service.getByNameInclude(this.param).subscribe((artist) => (this.l_artists = artist));
+    if (choice == 'nameExclude')this.service.getByNameExclude(this.param).subscribe((artist) => (this.l_artists = artist));
+    if (choice == 'ageGreat')this.service.getByAgeGreat(Number(this.param)).subscribe((artist) => (this.l_artists = artist));
+    if (choice == 'ageLess')this.service.getByAgeGreat(Number(this.param)).subscribe((artist) => (this.l_artists = artist));
+    if (choice == 'countryInclude')this.service.getByCountryInclude(this.param).subscribe((artist) => (this.l_artists = artist));
+    if (choice == 'countryExclude')this.service.getByCountryExclude(this.param).subscribe((artist) => (this.l_artists = artist));
+
+    this.results = this.l_artists.length;
+
   }
 
 }
+
+
