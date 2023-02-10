f68101d3267822bf04d72a6f544cff5ea565c301
William Serna <williamsernac@hotmail.com>
2023-02-09 23:57:28 -0500

    Re-added Artist and Album mocks

--- src/app/mocks/artist.mock.ts
+++ src/app/mocks/artist.mock.ts
@@ -16,5 +16,70 @@ export const ARTISTS: IArtist[] = [
         age: 38,
         year_debut: 2017,
         total_albums: 4
+    },
+    {
+      id: 3,
+      name: 'Eminem',
+      country: 'USA',
+      age: 50,
+      year_debut: 1988,
+      total_albums: 36
+    },
+    {
+      id: 4,
+      name: 'Daft Punk',
+      country: 'France',
+      age: 30,
+      year_debut: 1993,
+      total_albums: 4
+    },
+    {
+      id: 5,
+      name: 'Shakira',
+      country: 'Colombia',
+      age: 46,
+      year_debut: 1990,
+      total_albums: 11
+    },
+    {
+      id: 6,
+      name: 'The Weekend',
+      country: 'Canada',
+      age: 32,
+      year_debut: 2009,
+      total_albums: 5
+    },
+    {
+      id: 7,
+      name: 'Macklemore',
+      country: 'USA',
+      age: 39,
+      year_debut: 2000,
+      total_albums: 2
+    },
+    {
+      id: 8,
+      name: 'Shaggy',
+      country: 'Jamaica',
+      age: 54,
+      year_debut: 1992,
+      total_albums: 12
+    },
+    {
+      id: 9,
+      name: 'Drake',
+      country: 'Canada',
+      age: 36,
+      year_debut: 2001,
+      total_albums: 7
+    },
+    {
+      id: 10,
+      name: 'The Chainsmokers',
+      country: 'USA',
+      age: 9,
+      year_debut: 2012,
+      total_albums: 3
     }
+
 ]
