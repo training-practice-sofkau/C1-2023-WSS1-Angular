fead7dedc76d8458be7f6b3a06c1e92a68b4a591
Juangmcz <juangui-089@hotmail.com>
2023-02-10 00:51:11 -0500

    Artist component working as expected

--- src/app/components/artist-list/artist-list.component.ts
+++ src/app/components/artist-list/artist-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ARTISTS } from 'src/app/mocks/artist.mock';
 import { IArtist } from 'src/app/models/artist.interface';
+import { ArtistsService } from 'src/app/services/artist-service/artists.service';
 
 @Component({
   selector: 'app-artist-list',
@@ -11,40 +12,54 @@ export class ArtistListComponent implements OnInit {
   page: number = 1;
   param: string = '';
   results: number = 0;
+  filter: string = '';
+  typeSearch: string = '';
   artistsList: IArtist[] = [];
+  typeSearchList: string[] = ['Name', 'Age', 'Country'];
+  filtersList: string[] = [
+    'Starts with',
+    'Not starts with',
+    'Greater than',
+    'Less than',
+  ];
+
+  constructor(private service: ArtistsService) {}
 
   ngOnInit(): void {
-    this.artistsList = ARTISTS;
+    this.service.getAll().subscribe((artist) => (this.artistsList = artist));
     this.results = this.artistsList.length;
   }
 
-  filter(typeSearch: string) {
-    if (typeSearch == 'name') {
-      const filteredList = this.artistsList.filter((item) =>
-        item.name.toLowerCase().startsWith(this.param.toLocaleLowerCase())
-      );
-      this.results = filteredList.length;
-      this.artistsList = filteredList.sort((x, y) =>
-        x.name.localeCompare(y.name)
-      );
+  ngOnSearch() {
+    this.artistsList = ARTISTS;
+
+    switch (this.typeSearch) {
+      case 'Name': {
+        this.service
+          .getByName(this.param, this.filter)
+          .subscribe((artists) => (this.artistsList = artists));
+        break;
+      }
+
+      case 'Age': {
+        this.service
+          .getByAge(this.param, this.filter)
+          .subscribe((artists) => (this.artistsList = artists));
+        break;
+      }
+
+      case 'Country': {
+        this.service
+          .getByCountry(this.param, this.filter)
+          .subscribe((artists) => (this.artistsList = artists));
+        break;
+      }
+
+      default: {
+        this.artistsList = ARTISTS;
+        break;
+      }
     }
-    if (typeSearch == 'age') {
-      const filteredList = this.artistsList.filter(
-        (item) => item.age == parseInt(this.param)
-      );
-      this.results = filteredList.length;
-      this.artistsList = filteredList.sort((x, y) =>
-        x.name.localeCompare(y.name)
-      );
-    }
-    if (typeSearch == 'country') {
-      const filteredList = this.artistsList.filter((item) =>
-        item.country.toLowerCase().startsWith(this.param.toLocaleLowerCase())
-      );
-      this.results = filteredList.length;
-      this.artistsList = filteredList.sort((x, y) =>
-        x.name.localeCompare(y.name)
-      );
-    }
+    this.results = this.artistsList.length;
   }
 }
