ab9534448f3f1d1ed49c2b2c2f249a462fb05adb
andrscc <andrescc143@gmail.com>
2023-02-10 08:56:42 -0500

    The HTML content, styles and logic were added to the branch according to the previous progress made in WS for the Artist part of the app

--- src/app/components/artist-list/artist-list.component.ts
+++ src/app/components/artist-list/artist-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ARTISTS } from 'src/app/mocks/artist.mock';
 import { IArtist } from 'src/app/models/artist.interface';
-import { ArtistService } from 'src/app/services/artist-services/artist.service';
 
 @Component({
   selector: 'app-artist-list',
@@ -11,23 +10,34 @@ import { ArtistService } from 'src/app/services/artist-services/artist.service';
 
 export class ArtistListComponent implements OnInit{
 
-  constructor(private service: ArtistService) {}
-
-  @Input() param: string = "";
+  filterOption: string = "";
+  @Input() filterParam: string = "";
 
   //TO-DO: Define a variable that will store the information
-  l_artists: IArtist[] = [];
+  artistList: IArtist[] = [];
 
-  results: number = 0;
-  
+  results: IArtist[] = []
+
   ngOnInit(): void {
-    this.service.getAll().subscribe((artist) => this.l_artists = artist);
-    this.results = this.l_artists.length;
+    this.artistList = ARTISTS;
+    this.results = ARTISTS;
   }
 
   //TO-DO: Create a function that based of param it will show n-results
-  ngOnSearch(param: string, typeSearch: string){
-    console.log(param)
+  ngOnSearch(){
+    switch(this.filterOption){
+      case "name": {
+        this.results = this.artistList.filter(artist => artist.name.startsWith(this.filterParam));
+        break;
+      }
+      case "genre": {
+        this.results = this.artistList.filter(artist => artist.genre.startsWith(this.filterParam));
+        break;
+      }
+      case "albums": {
+        this.results = this.artistList.sort((a, b) => (a.albums > b.albums) ? -1 : 1);
+        break;
+      }
+    }
   }
-
 }
