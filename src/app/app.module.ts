01b2134871d7747fc476d44315f6fbb5db578d44
Juangmcz <juangui-089@hotmail.com>
2023-02-13 15:15:43 -0500

    Navigation side bar was implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AlbumListComponent } from './components/album-list/album-list.component';
 import { ArtistListComponent } from './components/artist-list/artist-list.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
 import { NotFoundComponent } from './components/pages/not-found/not-found.component';
+import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 @NgModule({
   declarations: [
@@ -17,8 +30,26 @@ import { NotFoundComponent } from './components/pages/not-found/not-found.compon
     AlbumListComponent,
     HomePageComponent,
     NotFoundComponent,
+    SidenavbarComponent,
+    WelcomeComponent,
   ],
-  imports: [FormsModule, BrowserModule, NgxPaginationModule, AppRoutingModule],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    NgxPaginationModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
