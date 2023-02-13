0b62f506303cec746b41fd344e3b2e9bd5ca513c
Julian Rojas <julianrojasgeo@gmail.com>
2023-02-13 13:46:48 -0500

    Material modules imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatSelectModule} from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { ArtistListComponent } from './components/artist-list/artist-list.component';
 import { AlbumListComponent } from './components/album-list/album-list.component';
-import { FormsModule } from '@angular/forms';
 import { AlbumComponent } from './components/album/album.component';
 import { ArtistComponent } from './components/artist/artist.component';
-import { NgxPaginationModule } from 'ngx-pagination';
 import { ListsContainerComponent } from './components/lists-container/lists-container.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
 import { ArtistListMenuComponent } from './components/artist-list-menu/artist-list-menu.component';
 import { AlbumListMenuComponent } from './components/album-list-menu/album-list-menu.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -37,6 +47,16 @@ import {MatSelectModule} from '@angular/material/select';
     NgxPaginationModule,
     BrowserAnimationsModule,
     MatSelectModule,
+    FormsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
