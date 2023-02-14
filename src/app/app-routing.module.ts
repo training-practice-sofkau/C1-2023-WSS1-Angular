8e3483da99a471004b1dabbe00dcfbfe5b7bc085
isan-san <isan.9.9f@gmail.com>
2023-02-13 23:36:42 -0500

    Create navigation bar and adapt styles to new design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,33 +4,37 @@ import { AppComponent } from './app.component';
 import { AlbumListComponent } from './components/album-list/album-list.component';
 import { ArtistListComponent } from './components/artist-list/artist-list.component';
 
-
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'home'
-
+    redirectTo: 'home',
   },
   {
     path: 'home',
     children: [
       {
         path: 'artist',
-        component: ArtistListComponent
+        component: ArtistListComponent,
       },
       {
         path: 'album',
-        component: AlbumListComponent
-      }
-
-    ]
+        component: AlbumListComponent,
+      },
+      {
+        path: 'new-artist',
+        component: ArtistListComponent,
+      },
+      {
+        path: 'new-album',
+        component: AlbumListComponent,
+      },
+    ],
   },
-
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
