3d27de16a7cb90bd2889782c5748fae940fb5865
isan-san <isan.9.9f@gmail.com>
2023-02-13 23:40:00 -0500

    Create new artist form

---
+++ src/app/components/forms/new-artist-form/new-artist-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-artist-form',
+  templateUrl: './new-artist-form.component.html',
+  styleUrls: ['./new-artist-form.component.scss']
+})
+export class NewArtistFormComponent {
+
+}
