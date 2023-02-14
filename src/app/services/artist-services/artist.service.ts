0950355e031517921b4f2ef6acf8bbdd62951897
William Serna <williamsernac@hotmail.com>
2023-02-14 11:50:16 -0500

    Code cleaning and Padding added to dialog

--- src/app/services/artist-services/artist.service.ts
+++ src/app/services/artist-services/artist.service.ts
@@ -14,32 +14,20 @@ export class ArtistService {
   //API url
   api: string = "http://localhost:8090/charts/artists"
 
-  //TO-DO: All the functionalities related to artist
-  /*getAll(): Observable<IArtist[]> {
 
-    let obsArtist: Observable<IArtist[]> = new Observable(observer => {
-      observer.next(ARTISTS);
-      observer.complete();
-    });
-
-    return obsArtist;
-
-  }*/
   getAll(): Observable<any> {
     return this.http.get(this.api);
   }
 
-
-
   getById(id: string): Observable<any>{
     //return this.http.get(`${this.api}/${id}`);
     return this.http.get(this.api+"/"+id);
-
   }
 
   postArtist(artist: IArtist){
     return this.http.post(this.api,artist);
   }
+
   putArtist(artist: IArtist){
     return this.http.put(this.api,artist);
   }
